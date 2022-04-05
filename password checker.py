e71e59f396f33f3a2762340523de3af3508c67f4
zhang <zhangmingxuan162@gmail.com>
2022-04-05 11:52:36 +0800

    Initial commit

---
+++ password checker.py
@@ -0,0 +1,11 @@
+"""
+Name: Mingxuan Zhang
+"""
+MINIMUM_LENGTH = 8
+def main():
+    password = input("Please enter a password at least {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < 8:
+        print("Password is too short!")
+        password = input("Pleas enter a password at least {} characters: ".format(MINIMUM_LENGTH))
+    print("*" * len(password))
+main()
