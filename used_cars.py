2b123b276f84dc026607dacdc3444b05434864d1
zhang <zhangmingxuan162@gmail.com>
2022-05-03 20:33:08 +0800

    Initial commit

---
+++ used_cars.py
@@ -0,0 +1,24 @@
+from prac_06.car import Car
+
+import car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car(100)
+    limo.add_fule(20)
+    print(limo.fule)
+    limo.drive(115)
+    print(limo.odometer)
+
+
+main()
