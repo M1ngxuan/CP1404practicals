f8b172ae9f411cb5c9bf8f84345a2d67ebc90ef1
zhang <zhangmingxuan162@gmail.com>
2022-05-10 22:02:41 +0800

    Initial commit

---
+++ convert_miles_km.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+M_TO_KM = 1.60934
+
+class MileConverterApp(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+    def handle_calculate(self):
+        value = self.get_validated_miles()
+        result = value * M_TO_KM
+        self.root.ids.output_label.text = str(result)
+    def handle_increment(self, change):
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+    def get_validated_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+MileConverterApp().run()
